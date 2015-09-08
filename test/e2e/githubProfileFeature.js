ee6a0f4aca615b4a1f3c7e72e1f45ceea37468cd
raphlevy <raphlevy.perso@gmail.com>
2015-09-08 17:16:05 +0100

    Completed step 1

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,19 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
+  it('finds profiles', function() {
     browser.get('http://localhost:8080');
 
-    expect(browser.getTitle()).toEqual('Github user search');
+    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('user.login')).getText()).
+      toEqual('spike01');
+
   });
+
+
+  // it('has a title', function() {
+  //   browser.get('http://localhost:8080');
+
+  //   expect(browser.getTitle()).toEqual('Github user search');
+  // });
 });
