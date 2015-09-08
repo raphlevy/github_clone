1e0f0a8d2e8954757ab8622a5ec7245e53f57727
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-08 10:08:17 +0100

    Started making first test

---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,9 @@
+describe('GitUserSearchController', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
+});
