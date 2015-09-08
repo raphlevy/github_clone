c40b67c877f114efff3deeb88cdfdebef6c13152
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-08 10:54:36 +0100

    Created a controller and first test passing

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -6,4 +6,9 @@ describe('GitUserSearchController', function() {
   beforeEach(inject(function($controller) {
     ctrl = $controller('GitUserSearchController');
   }));
+
+  it('initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
 });
